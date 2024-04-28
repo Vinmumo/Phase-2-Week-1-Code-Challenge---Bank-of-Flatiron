2e532870a31580f9621a7839c7c6c8274583f712
vincent <vinnymummo@gmail.com>
2024-04-28 20:32:08 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Table from './Table';
+import Header from './Header';
 
 function App() {
   return (
    <div className='container'>
+    <Header />
    <Table />
    </div>
   );
--- src/Table.js
+++ src/Table.js
@@ -2,30 +2,31 @@ import React from "react";
 import Row from "./Row";
 import { useState } from "react";
 import Search from "./Search";
+import Form from "./Form";
 
 export default function Table(){
 
 let transactions = [
     {
-        date: "01/02/2002",
+        date: "2002-02-12",
         description: "Debts",
         category: "income",
         amount: "$300"
     },
     {
-        date: "10/04/2022",
+        date: "2032-05-16",
         description: "Business",
         category: "expense",
         amount: "$600"
     },
     {
-        date: "11/03/2012",
+        date: "2012-03-11",
         description: "Loan",
         category: "expense",
         amount: "$700"
     },
     {
-        date: "21/11/2023",
+        date: "2023-11-21",
         description: "Lucky draw",
         category: "income",
         amount: "$1000"
@@ -37,6 +38,7 @@ const [transactionn, setTransactionn] = useState(transactions)
 return (
     <>
   <Search transactionn={transactionn} setTransactionn = {setTransactionn} transactions = {transactions}/>
+  <Form transactionn={transactionn} setTransactionn = {setTransactionn} />
     <table className="table table-secondary">
 <thead>
    <tr>
